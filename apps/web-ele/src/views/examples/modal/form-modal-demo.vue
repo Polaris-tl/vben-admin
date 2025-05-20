abb547fe4c0a076ad5aba4d00f58f0b127eea820
polaris <polaris66tl@gmail.com>
2025-05-20 16:58:15 +0800

    feat: 弹窗示例

---
+++ apps/web-ele/src/views/examples/modal/form-modal-demo.vue
@@ -0,0 +1,94 @@
+<script lang="ts" setup>
+import { useVbenModal } from '@vben/common-ui';
+
+import { ElMessage } from 'element-plus';
+
+import { useVbenForm } from '#/adapter/form';
+
+defineOptions({
+  name: 'FormModelDemo',
+});
+
+const [Form, formApi] = useVbenForm({
+  handleSubmit: onSubmit,
+  schema: [
+    {
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入',
+      },
+      fieldName: 'field1',
+      label: '字段1',
+      rules: 'required',
+    },
+    {
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入',
+      },
+      fieldName: 'field2',
+      label: '字段2',
+      rules: 'required',
+    },
+    {
+      component: 'Select',
+      componentProps: {
+        options: [
+          { label: '选项1', value: '1' },
+          { label: '选项2', value: '2' },
+        ],
+        placeholder: '请输入',
+      },
+      fieldName: 'field3',
+      label: '字段3',
+      rules: 'required',
+    },
+  ],
+  showDefaultActions: false,
+});
+
+const [Modal, modalApi] = useVbenModal({
+  fullscreenButton: false,
+  onCancel() {
+    modalApi.close();
+  },
+  onConfirm: async () => {
+    await formApi.validateAndSubmitForm();
+    // modalApi.close();
+  },
+  onOpenChange(isOpen: boolean) {
+    if (isOpen) {
+      const { values } = modalApi.getData<Record<string, any>>();
+      if (values) {
+        formApi.setValues(values);
+      }
+    }
+  },
+  title: '内嵌表单示例',
+});
+
+function onSubmit(values: Record<string, any>) {
+  const loadingMsg = ElMessage({
+    message: '正在提交中...',
+    type: 'info',
+    duration: 0,
+    showClose: true,
+  });
+  modalApi.lock();
+  setTimeout(() => {
+    modalApi.close();
+    loadingMsg.close && loadingMsg.close();
+    ElMessage({
+      message: `提交成功：${JSON.stringify(values)}`,
+      type: 'success',
+      duration: 2000,
+      showClose: true,
+    });
+  }, 3000);
+}
+</script>
+<template>
+  <Modal>
+    <Form />
+  </Modal>
+</template>
